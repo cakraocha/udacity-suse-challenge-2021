59947bfd05f29d331e0a186d2779e3d58cc71f18
root <root@MSI.localdomain>
2021-06-24 17:55:43 +1000

    add status and metrics to exercise 1

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,36 @@
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
+
+# Create a routing for the healthcheck in /status
+@app.route("/status")
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+# Create a routing for metrics in /metrics
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success",
+                                "code":0,
+                                "data":{"UserCount":140,
+                                        "UserCountActive":23
+                                        }
+                                }
+                                ),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
 
 @app.route("/")
 def hello():
